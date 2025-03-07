72eb9abae230ad767f7615f0e1b2e641468a0e06
Saeed Kohansal <saeedkohan8520@gmail.com>
2025-03-07 17:17:10 +0330

    Create PDF folder

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: H:/C+project/chess_C
+# Install script for directory: H:/C+project/Chess_C++/Chess
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "H:/C+project/chess_C/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "H:/C+project/Chess_C++/Chess/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
